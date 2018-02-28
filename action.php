ae5282056fde4b3acb4b33d721c80124a1ce850e
sho23 <testuser.shota@gmail.com>
2018-02-28 21:27:41 +0900

    メールの文章を追加

--- action.php
+++ action.php
@@ -98,12 +98,10 @@ require_once __DIR__ . "/vendor/autoload.php";
 				mb_internal_encoding("UTF-8");
 				$to = $_ENV['MAIL_TO'];
 				$subject = 'ビジター申請がありました';
-				$message = '本文';
+				$message = 'ビジター申請がありました。<br>内容は以下の通りです。<br><br><br>■名前: '. $name .'<br>■部屋番号: '. $room .'<br>■訪問/宿泊: '. $visitType .'<br>■人数: '. $number .'<br>■日時: '. $date . $time;
 				$from  = "From: ". $_ENV['MAIL_FROM'] . "\r\n";
 				$from .= "Return-Path: " . $_ENV['MAIL_FROM'];
-				if(mb_send_mail($to, $subject, $message, $from)){
-					echo "メールを送信しました";
-				} else {
+				if(!mb_send_mail($to, $subject, $message, $from)){
 					echo "メールの送信に失敗しました";
 				}
 		    }else{
